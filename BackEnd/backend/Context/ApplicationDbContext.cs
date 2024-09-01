0090599019cadfacdfaede023a4b5c01219ee5de
Gilang Aji <93376953+Fuhako@users.noreply.github.com>
2024-09-01 12:26:27 +0700

    Add backend
    add backend

---
+++ BackEnd/backend/Context/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.ProductModule.Model;
+namespace backend.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<ProductModel> Products { get; set; }
+    }
+}
---
+++ BackEnd/backend/ProductVariantModule/Model/ProductVariant.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.ProductVariantModule.Model
+{
+    public class ProductVariantModel
+    {
+        [Key]
+        public long id { get; set; }
+        public long product_id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+        public long qty { get; set; }
+        public decimal price { get; set; }
+        public bool active { get; set; }
+        public string created_user { get; set; }
+        public DateTime created_date { get; set; }
+        public string updated_user { get; set; }
+        public string updated_date { get; set; }
+    }
+}
